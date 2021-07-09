c19ecf3c9aa45d98041419a1594b09b3d6051390
YPINPIN <yping611@gmail.com>
2021-07-09 18:22:59 +0800

    [ADD] Add restaurant schema

---
+++ models/restaurant.js
@@ -0,0 +1,50 @@
+// mongoose
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const restaurantSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  name_en: {
+    type: String,
+    trim: true,
+  },
+  category: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  image: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  location: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  phone: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  google_map: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+})
+module.exports = mongoose.model('Restaurant', restaurantSchema)
